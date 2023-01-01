15a9cc121cdcecb1cc52096a61efe7056eef92b1
EinMilchBoss <christian.benezuela@gmail.com>
2023-01-01 20:28:16 +0100

    Add 20221202 part 2.

--- src/bin/2022/02/main.rs
+++ src/bin/2022/02/main.rs
@@ -1,12 +1,36 @@
 use std::cmp::Ordering;
+use std::ops::RangeInclusive;
 use std::str::FromStr;
 
 use util::std::*;
+  
+use num_derive::FromPrimitive;
+use num_traits::FromPrimitive;
 
 const YEAR: Year = Year("2022");
 const DAY: Day = Day("02");
 
-#[derive(Clone, Copy, PartialEq)]
+#[derive(Clone, Copy)]
+enum Outcome {
+    Win = 6,
+    Draw = 3,
+    Lose = 0,
+}
+
+impl FromStr for Outcome {
+    type Err = String;
+    
+    fn from_str(outcome: &str) -> Result<Self, <Self as FromStr>::Err> {
+        match outcome {
+            "X" => Ok(Self::Lose),
+            "Y" => Ok(Self::Draw),
+            "Z" => Ok(Self::Win),
+            _ => Err(format!("Outcome {} could not be parsed.", outcome)),
+        }
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, FromPrimitive)]
 enum Command {
     Rock = 1,
     Paper = 2,
@@ -21,7 +45,7 @@ impl FromStr for Command {
             "A" | "X" => Ok(Self::Rock),
             "B" | "Y" => Ok(Self::Paper),
             "C" | "Z" => Ok(Self::Scissors),
-            _ => Err("Command {command} could not be parsed.".to_string()),
+            _ => Err(format!("Command {} could not be parsed.", command)),
         }
     }
 }
@@ -40,24 +64,68 @@ impl PartialOrd for Command {
     }
 }
 
-struct Round {
+struct CommandRound {
     opponent: Command,
     player: Command,
 }
 
-impl Round {
+impl CommandRound {
     fn new(opponent: &Command, player: &Command) -> Self {
-        Round {
+        CommandRound {
             opponent: *opponent,
             player: *player,
         }
     }
 
-    fn points(&self) -> u8 {
+    fn round_outcome(&self) -> Outcome {
         match self.player.partial_cmp(&self.opponent).unwrap() {
-            Ordering::Greater => 6,
-            Ordering::Equal => 3,
-            Ordering::Less => 0,
+            Ordering::Greater => Outcome::Win,
+            Ordering::Equal => Outcome::Draw,
+            Ordering::Less => Outcome::Lose,
+        }
+    }
+}
+
+struct OutcomeRound {
+    opponent: Command,
+    outcome: Outcome,
+}
+
+impl OutcomeRound {
+    fn new(opponent: &Command, outcome: &Outcome) -> Self {
+        OutcomeRound {
+            opponent: *opponent,
+            outcome: *outcome,
+        }
+    }
+
+    fn player_command(&self) -> Command {
+        let mut player_value;
+        let opponent_value = self.opponent as u8;
+        match self.outcome {
+            Outcome::Win => {
+                player_value = opponent_value + 1;
+                cycle(1..=3, &mut player_value);
+                FromPrimitive::from_u8(player_value).unwrap()
+            },
+            Outcome::Lose => {
+                player_value = opponent_value - 1;
+                cycle(1..=3, &mut player_value);
+                FromPrimitive::from_u8(player_value).unwrap()
+            },
+            Outcome::Draw => self.opponent,
+        }
+    }
+}
+
+fn cycle(range: RangeInclusive<u8>, value: &mut u8) {
+    if *value > *range.end() {
+        while !range.contains(value) {
+            *value -= range.end();
+        }
+    } else if *value < *range.start() {
+        while !range.contains(value) {
+            *value += range.end();
         }
     }
 }
@@ -66,25 +134,41 @@ fn solve_first(input: &str) -> String {
     input
         .lines()
         .map(|line| {
-            let mut command_iter = line
+            let mut iter = line
                 .split(' ')
-                .map(|command| Command::from_str(command).expect("Command should be parsed."));
+                .map(|command| Command::from_str(command).unwrap());
             
-            let opponent = command_iter.next();
-            let player = command_iter.next();
+            let opponent = iter.next();
+            let player = iter.next();
             if opponent.is_none() || player.is_none() {
                 panic!("Command of opponent or player was None.");
             }
 
-            let rounds = Round::new(&opponent.unwrap(), &player.unwrap());
-            (rounds.points() + rounds.player as u8) as u16
+            let rounds = CommandRound::new(&opponent.unwrap(), &player.unwrap());
+            rounds.round_outcome() as u32 + rounds.player as u32
         })
-        .sum::<u16>()
+        .sum::<u32>()
         .to_string()
 }
 
-fn solve_second(_: &str) -> String {
-    "".to_string()
+fn solve_second(input: &str) -> String {
+    input
+        .lines()
+        .map(|line| {
+            let mut iter = line
+                .split(' ');
+
+            let opponent = Command::from_str(iter.next().unwrap());
+            let outcome = Outcome::from_str(iter.next().unwrap());
+            if opponent.is_err() || outcome.is_err() {
+                panic!("Command of opponent or outcome was Err.");
+            }
+
+            let rounds = OutcomeRound::new(&opponent.unwrap(), &outcome.unwrap());
+            rounds.player_command() as u32 + rounds.outcome as u32
+        })
+        .sum::<u32>()
+        .to_string()
 }
 
 fn main() {
@@ -93,7 +177,7 @@ fn main() {
 
     if let Some(example) = example {
         println!("First: Expected {} found {}.", 15, solve_first(&example));
-        println!("Second: Expected {} found {}.", 45_000, solve_second(&example));
+        println!("Second: Expected {} found {}.", 12, solve_second(&example));
     }
 
     if let Some(input) = input {
