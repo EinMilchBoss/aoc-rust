4c6523deba364fce0bda80d03e942732dfb4cb51
EinMilchBoss <christian.benezuela@gmail.com>
2023-01-06 17:59:09 +0100

    Refactor 221204.

--- src/bin/2022/04/main.rs
+++ src/bin/2022/04/main.rs
@@ -1,82 +1,52 @@
+use std::ops::RangeInclusive;
+
 use util::std::*;
 
 const YEAR: Year = Year("2022");
 const DAY: Day = Day("04");
 
-fn solve_first(input: &str) -> String {
-    let x = 3..=5;
-    // x.all(|part| y.contains(part))
-    // len() for the amount?
-    // filtering out non matching
+fn contains_all(outer: &RangeInclusive<u8>, inner: &RangeInclusive<u8>) -> bool {
+    outer.start() <= inner.start() && outer.end() >= inner.end()
+}
 
-    let parsed: Vec<_> = input
+fn contains_any(outer: &RangeInclusive<u8>, inner: &mut RangeInclusive<u8>) -> bool {
+    inner.any(|part| outer.contains(&part))
+}
+
+fn parse(input: &str) -> Vec<(RangeInclusive<u8>, RangeInclusive<u8>)> {
+    input
         .lines()
-        .map(|line| { 
-            let mut ranges = line
-                .split(',')
-                .map(|range| {
-                    let mut sides  = range
-                        .split('-')
-                        .map(|side| side.parse::<u8>().unwrap());
-                    let start = sides.next().unwrap();
-                    let end = sides.next().unwrap();
-                    start..=end
-                });
+        .map(|line| {
+            let mut ranges = line.split(',').map(|range| {
+                let mut sides = range.split('-').map(|side| side.parse::<u8>().unwrap());
+                let start = sides.next().unwrap();
+                let end = sides.next().unwrap();
+                start..=end
+            });
             (ranges.next().unwrap(), ranges.next().unwrap())
         })
-        .collect();
+        .collect()
+}
 
-    let overlapping: Vec<_> = parsed
+fn solve_first(input: &str) -> String {
+    parse(input)
         .iter()
         .cloned()
-        .filter(|(first, second)| {
-            let first_overlaps = first.start() <= second.start() && first.end() >= second.end();
-            let second_overlaps = second.start() <= first.start() && second.end() >= first.end();
-            first_overlaps || second_overlaps
-        })
-        .collect();
-
-    //dbg!(&overlapping);
-
-
-    
-    overlapping.len().to_string()
+        .filter(|(first, second)| contains_all(first, second) || contains_all(second, first))
+        .count()
+        .to_string()
 }
 
 fn solve_second(input: &str) -> String {
-    let parsed: Vec<_> = input
-        .lines()
-        .map(|line| { 
-            let mut ranges = line
-                .split(',')
-                .map(|range| {
-                    let mut sides  = range
-                        .split('-')
-                        .map(|side| side.parse::<u8>().unwrap());
-                    let start = sides.next().unwrap();
-                    let end = sides.next().unwrap();
-                    start..=end
-                });
-            (ranges.next().unwrap(), ranges.next().unwrap())
-        })
-        .collect();
-
-    let overlapping: Vec<_> = parsed
+    parse(input)
         .iter()
         .cloned()
         .filter(|range| {
-            let (mut first, mut second) = range.clone();
-            let first_overlaps = second.any(|part| first.contains(&part));
-            let second_overlaps = first.any(|part| second.contains(&part));
-            first_overlaps || second_overlaps
+            let (ref first, ref mut second) = range.clone();
+            contains_any(first, second)
         })
-        .collect();
-
-    //dbg!(&overlapping);
-
-
-    
-    overlapping.len().to_string()
+        .count()
+        .to_string()
 }
 
 fn main() {
@@ -84,16 +54,8 @@ fn main() {
     let input = read_file(InputFile::Actual, YEAR, DAY);
 
     if let Some(example) = example {
-        println!(
-            "First: Expected {} found {}.",
-            2,
-            solve_first(&example)
-        );
-        println!(
-            "Second: Expected {} found {}.",
-            45_000,
-            solve_second(&example)
-        );
+        println!("First: Expected {} found {}.", 2, solve_first(&example));
+        println!("Second: Expected {} found {}.", 4, solve_second(&example));
     }
 
     if let Some(input) = input {
