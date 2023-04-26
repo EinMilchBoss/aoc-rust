9ba9bca6d846a54b2089fae10d11b21f443544f1
EinMilchBoss <christian.benezuela@gmail.com>
2023-04-26 12:56:53 +0200

    Solve 20151206 both parts.

---
+++ src/bin/2015/06/parse.rs
@@ -0,0 +1,48 @@
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{char, digit1, newline},
+    combinator::{map_res, opt, recognize},
+    multi::many0,
+    sequence::pair,
+    IResult,
+};
+
+use super::*;
+
+fn parse_coordinate(input: &str) -> IResult<&str, Coordinate> {
+    let (input, x) = map_res(recognize(digit1), str::parse)(input)?;
+    let (input, _) = tag(",")(input)?;
+    let (input, y) = map_res(recognize(digit1), str::parse)(input)?;
+    Ok((input, Coordinate { x, y }))
+}
+
+fn parse_coordinate_pair(input: &str) -> IResult<&str, CoordinatePair> {
+    let (input, from_coordinate) = parse_coordinate(input)?;
+    let (input, _) = tag(" through ")(input)?;
+    let (input, to_coordinate) = parse_coordinate(input)?;
+    Ok((input, CoordinatePair(from_coordinate, to_coordinate)))
+}
+
+fn parse_command(input: &str) -> IResult<&str, Command> {
+    let (input, command_value) = alt((tag("turn on"), tag("turn off"), tag("toggle")))(input)?;
+    let (input, _) = char(' ')(input)?;
+    Ok((input, command_value.into()))
+}
+
+fn parse_instruction(input: &str) -> IResult<&str, Instruction> {
+    let (input, (command, coordinate_pair)) = pair(parse_command, parse_coordinate_pair)(input)?;
+    let (input, _) = opt(newline)(input)?;
+    Ok((
+        input,
+        Instruction {
+            command,
+            coordinate_pair,
+        },
+    ))
+}
+
+pub fn parse(input: &str) -> Vec<Instruction> {
+    let (_, instructions) = many0(parse_instruction)(input).expect("Parsing failed.");
+    instructions
+}
