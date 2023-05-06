8024647746b5246accafb4767610768b0eb2efd1
EinMilchBoss <christian.benezuela@gmail.com>
2023-05-06 22:23:21 +0200

    Fix parsing and edit data structures for parsed data.

--- src/bin/2015/07/main.rs
+++ src/bin/2015/07/main.rs
@@ -8,47 +8,184 @@ use util::std::*;
 const YEAR: Year = Year("2015");
 const DAY: Day = Day("07");
 
+// go through every instruction
+// if corresponding wires to all necessary input signals are given
+// execute instruction
+// else
+// add to unprocessed instructions
+// call for unprocessed instructions recursively
+
+// fn execute_instructions(wires: &mut HashMap<Wire, Signal>, instructions: Vec<Instruction>) {
+//     if instructions.is_empty() {
+//         return;
+//     }
+
+//     dbg!(wires.len());
+
+//     let mut unprocessed_instructions = Vec::new();
+//     for instruction in instructions {
+//         dbg!(&instruction);
+//         match instruction.command {
+//             Command::Set(new_signal) => {
+//                 *wires.entry(instruction.destination).or_insert(new_signal) = new_signal
+//             }
+//             Command::And(ref left, ref right) => {
+//                 let left = wires.get(left);
+//                 let right = wires.get(right);
+//                 if let (Some(left), Some(right)) = (left, right) {
+//                     let new_signal = Signal(left.0 & right.0);
+//                     *wires.entry(instruction.destination).or_insert(new_signal) = new_signal;
+//                 } else {
+//                     unprocessed_instructions.push(instruction);
+//                 }
+//             }
+//             Command::Or(ref left, ref right) => {
+//                 let left = wires.get(left);
+//                 let right = wires.get(right);
+//                 if let (Some(left), Some(right)) = (left, right) {
+//                     let new_signal = Signal(left.0 | right.0);
+//                     *wires.entry(instruction.destination).or_insert(new_signal) = new_signal;
+//                 } else {
+//                     unprocessed_instructions.push(instruction);
+//                 }
+//             }
+//             Command::LeftShift(ref wire, ref shift) => {
+//                 let wire = wires.get(wire);
+//                 if let Some(wire) = wire {
+//                     let new_signal = Signal(wire.0 << shift);
+//                     *wires.entry(instruction.destination).or_insert(new_signal) = new_signal;
+//                 } else {
+//                     unprocessed_instructions.push(instruction);
+//                 }
+//             }
+//             Command::RightShift(ref wire, shift) => {
+//                 let wire = wires.get(wire);
+//                 if let Some(wire) = wire {
+//                     let new_signal = Signal(wire.0 >> shift);
+//                     *wires.entry(instruction.destination).or_insert(new_signal) = new_signal;
+//                 } else {
+//                     unprocessed_instructions.push(instruction);
+//                 }
+//             }
+//             Command::Not(ref wire) => {
+//                 let wire = wires.get(wire);
+//                 if let Some(wire) = wire {
+//                     let new_signal = Signal(!wire.0);
+//                     *wires.entry(instruction.destination).or_insert(new_signal) = new_signal;
+//                 } else {
+//                     unprocessed_instructions.push(instruction);
+//                 }
+//             }
+//         };
+//     }
+
+//     execute_instructions(wires, unprocessed_instructions);
+// }
+
+// fn execute_set_instructions(
+//     wires: &mut HashMap<Wire, Signal>,
+//     instructions: Vec<Instruction>,
+// ) -> Vec<Instruction> {
+//     let mut unprocessed = Vec::new();
+//     for instruction in instructions {
+//         if let Command::Set(new_signal) = instruction.command {
+//             dbg!(&instruction);
+//             *wires.entry(instruction.destination).or_insert(new_signal) = new_signal;
+//         } else {
+//             unprocessed.push(instruction);
+//         }
+//     }
+//     unprocessed
+// }
+
 fn solve_first(input: &str) -> String {
     let instructions = parse_instructions(input).unwrap();
-    let mut wires: HashMap<Wire, Signal> = HashMap::new();
-    for instruction in instructions {
-        match instruction.command {
-            Command::Set(new_signal) => {
-                *wires.entry(instruction.destination).or_insert(new_signal) = new_signal
-            }
-            Command::And(left, right) => {
-                let left = wires.get(&left).unwrap();
-                let right = wires.get(&right).unwrap();
-                let new_signal = Signal(left.0 & right.0);
-                *wires.entry(instruction.destination).or_insert(new_signal) = new_signal;
-            }
-            Command::Or(left, right) => {
-                let left = wires.get(&left).unwrap();
-                let right = wires.get(&right).unwrap();
-                let new_signal = Signal(left.0 | right.0);
-                *wires.entry(instruction.destination).or_insert(new_signal) = new_signal;
-            }
-            Command::LeftShift(wire, shift) => {
-                let wire = wires.get(&wire).unwrap();
-                let new_signal = Signal(wire.0 << shift);
-                *wires.entry(instruction.destination).or_insert(new_signal) = new_signal;
-            }
-            Command::RightShift(wire, shift) => {
-                let wire = wires.get(&wire).unwrap();
-                let new_signal = Signal(wire.0 >> shift);
-                *wires.entry(instruction.destination).or_insert(new_signal) = new_signal;
-            }
-            Command::Not(wire) => {
-                let wire = wires.get(&wire).unwrap();
-                let new_signal = Signal(!wire.0);
-                *wires.entry(instruction.destination).or_insert(new_signal) = new_signal;
-            }
-        };
-    }
-
-    dbg!(wires);
+    dbg!(instructions.len());
+    dbg!(&instructions);
 
     "".to_string()
+
+    // // first execute all set commands
+    // // check if all inputs to a gate are already in "wires" map
+    // // remove executed instructions
+
+    // let mut wires: HashMap<Wire, Signal> = HashMap::new();
+
+    // let instructions = parse_instructions(input).unwrap();
+    // dbg!(instructions.len());
+    // dbg!(&instructions);
+    // let remaining_instructions = execute_set_instructions(&mut wires, instructions);
+
+    // execute_instructions(&mut wires, remaining_instructions);
+
+    // // instructions.iter().for_each(|instruction| {
+    // //     if let Command::Set(new_signal) = instruction.command {
+    // //         *wires
+    // //             .entry(instruction.destination.clone())
+    // //             .or_insert(new_signal) = new_signal
+    // //     }
+    // // });
+
+    // // let mut unprocessed_instructions = Vec::new();
+
+    // // for instruction in instructions {
+    // //     match instruction.command {
+    // //         Command::Set(new_signal) => {
+    // //             *wires.entry(instruction.destination).or_insert(new_signal) = new_signal
+    // //         }
+    // //         Command::And(ref left, ref right) => {
+    // //             let left = wires.get(left);
+    // //             let right = wires.get(right);
+    // //             if let (Some(left), Some(right)) = (left, right) {
+    // //                 let new_signal = Signal(left.0 & right.0);
+    // //                 *wires.entry(instruction.destination).or_insert(new_signal) = new_signal;
+    // //             } else {
+    // //                 unprocessed_instructions.push(instruction);
+    // //             }
+    // //         }
+    // //         Command::Or(ref left, ref right) => {
+    // //             let left = wires.get(left);
+    // //             let right = wires.get(right);
+    // //             if let (Some(left), Some(right)) = (left, right) {
+    // //                 let new_signal = Signal(left.0 | right.0);
+    // //                 *wires.entry(instruction.destination).or_insert(new_signal) = new_signal;
+    // //             } else {
+    // //                 unprocessed_instructions.push(instruction);
+    // //             }
+    // //         }
+    // //         Command::LeftShift(ref wire, ref shift) => {
+    // //             let wire = wires.get(wire);
+    // //             if let Some(wire) = wire {
+    // //                 let new_signal = Signal(wire.0 << shift);
+    // //                 *wires.entry(instruction.destination).or_insert(new_signal) = new_signal;
+    // //             } else {
+    // //                 unprocessed_instructions.push(instruction);
+    // //             }
+    // //         }
+    // //         Command::RightShift(ref wire, shift) => {
+    // //             let wire = wires.get(wire);
+    // //             if let Some(wire) = wire {
+    // //                 let new_signal = Signal(wire.0 >> shift);
+    // //                 *wires.entry(instruction.destination).or_insert(new_signal) = new_signal;
+    // //             } else {
+    // //                 unprocessed_instructions.push(instruction);
+    // //             }
+    // //         }
+    // //         Command::Not(ref wire) => {
+    // //             let wire = wires.get(wire);
+    // //             if let Some(wire) = wire {
+    // //                 let new_signal = Signal(!wire.0);
+    // //                 *wires.entry(instruction.destination).or_insert(new_signal) = new_signal;
+    // //             } else {
+    // //                 unprocessed_instructions.push(instruction);
+    // //             }
+    // //         }
+    // //     };
+    // // }
+
+    // dbg!(&wires);
+
+    // wires.get(&Wire("a".to_string())).unwrap().0.to_string()
 }
 
 fn solve_second(input: &str) -> String {
--- src/bin/2015/07/parser.rs
+++ src/bin/2015/07/parser.rs
@@ -2,49 +2,63 @@ use nom::{
     branch::alt,
     bytes::complete::tag,
     character::complete::{alpha1, digit1, newline},
-    combinator::map_res,
+    combinator::{cut, map_res},
     multi::separated_list0,
     sequence::{preceded, separated_pair},
     Finish, IResult,
 };
 
-#[derive(Debug)]
+type SignalSize = u16;
+
+#[derive(Debug, PartialEq, Eq)]
 pub struct Instruction {
     pub command: Command,
     pub destination: Wire,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Command {
-    Set(Signal),
-    And(Wire, Wire),
-    Or(Wire, Wire),
-    LeftShift(Wire, u16),
-    RightShift(Wire, u16),
-    Not(Wire),
+    Set(CommandInput),
+    And(CommandInput, CommandInput),
+    Or(CommandInput, CommandInput),
+    LeftShift {
+        value: CommandInput,
+        shift: SignalSize,
+    },
+    RightShift {
+        value: CommandInput,
+        shift: SignalSize,
+    },
+    Not(CommandInput),
 }
 
-#[derive(Debug, Clone, Copy)]
-pub struct Signal(pub u16);
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum CommandInput {
+    Signal(Signal),
+    Wire(Wire),
+}
 
-#[derive(Debug, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Signal(pub SignalSize);
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Wire(pub String);
 
-pub fn parse_instructions(input: &str) -> Result<Vec<Instruction>, nom::error::Error<&str>> {
-    separated_list0(newline, parse_instruction)(input)
+pub fn parse_instructions(input: &str) -> Result<Vec<Instruction>, nom::error::VerboseError<&str>> {
+    separated_list0(newline, cut(parse_instruction))(input)
         .finish()
         .map(|(_, instructions)| instructions)
 }
 
-fn parse_instruction(input: &str) -> IResult<&str, Instruction> {
+fn parse_instruction(input: &str) -> IResult<&str, Instruction, nom::error::VerboseError<&str>> {
     let (input, (command, destination)) = separated_pair(
         alt((
-            parse_set,
-            parse_and,
-            parse_or,
-            parse_left_shift,
-            parse_right_shift,
-            parse_not,
+            parse_command_and,
+            parse_command_or,
+            parse_command_left_shift,
+            parse_command_right_shift,
+            parse_command_not,
+            parse_command_set,
         )),
         tag(" -> "),
         parse_wire,
@@ -58,54 +72,130 @@ fn parse_instruction(input: &str) -> IResult<&str, Instruction> {
     ))
 }
 
-fn parse_set(input: &str) -> IResult<&str, Command> {
-    let (input, signal) = parse_signal(input)?;
-    Ok((input, Command::Set(signal)))
+fn parse_command_set(input: &str) -> IResult<&str, Command, nom::error::VerboseError<&str>> {
+    let (input, command_input) = parse_command_input(input)?;
+    Ok((input, Command::Set(command_input)))
 }
 
-fn parse_and(input: &str) -> IResult<&str, Command> {
-    let (input, (left, right)) = parse_gate_command(" AND ")(input)?;
+fn parse_command_and(input: &str) -> IResult<&str, Command, nom::error::VerboseError<&str>> {
+    let (input, (left, right)) = parse_separated_command_input_pair(" AND ")(input)?;
     Ok((input, Command::And(left, right)))
 }
 
-fn parse_or(input: &str) -> IResult<&str, Command> {
-    let (input, (left, right)) = parse_gate_command(" OR ")(input)?;
+fn parse_command_or(input: &str) -> IResult<&str, Command, nom::error::VerboseError<&str>> {
+    let (input, (left, right)) = parse_separated_command_input_pair(" OR ")(input)?;
     Ok((input, Command::Or(left, right)))
 }
 
-fn parse_left_shift(input: &str) -> IResult<&str, Command> {
-    let (input, (left, right)) = parse_shift_command(" LSHIFT ")(input)?;
-    Ok((input, Command::LeftShift(left, right)))
+fn parse_command_left_shift(input: &str) -> IResult<&str, Command, nom::error::VerboseError<&str>> {
+    let (input, (value, shift)) = parse_shift_command(" LSHIFT ")(input)?;
+    Ok((input, Command::LeftShift { value, shift }))
 }
 
-fn parse_right_shift(input: &str) -> IResult<&str, Command> {
-    let (input, (left, right)) = parse_shift_command(" RSHIFT ")(input)?;
-    Ok((input, Command::RightShift(left, right)))
+fn parse_command_right_shift(
+    input: &str,
+) -> IResult<&str, Command, nom::error::VerboseError<&str>> {
+    let (input, (value, shift)) = parse_shift_command(" RSHIFT ")(input)?;
+    Ok((input, Command::RightShift { value, shift }))
 }
 
-fn parse_not(input: &str) -> IResult<&str, Command> {
-    let (input, wire) = preceded(tag("NOT "), parse_wire)(input)?;
-    Ok((input, Command::Not(wire)))
+fn parse_command_not(input: &str) -> IResult<&str, Command, nom::error::VerboseError<&str>> {
+    let (input, command_input) = preceded(tag("NOT "), parse_command_input)(input)?;
+    Ok((input, Command::Not(command_input)))
 }
 
-fn parse_gate_command(separator: &str) -> impl Fn(&str) -> IResult<&str, (Wire, Wire)> + '_ {
-    move |input: &str| separated_pair(parse_wire, tag(separator), parse_wire)(input)
+fn parse_separated_command_input_pair(
+    separator: &str,
+) -> impl Fn(&str) -> IResult<&str, (CommandInput, CommandInput), nom::error::VerboseError<&str>> + '_
+{
+    move |input: &str| {
+        separated_pair(parse_command_input, tag(separator), parse_command_input)(input)
+    }
 }
 
-fn parse_shift_command(separator: &str) -> impl Fn(&str) -> IResult<&str, (Wire, u16)> + '_ {
-    move |input: &str| separated_pair(parse_wire, tag(separator), parse_u16)(input)
+fn parse_shift_command(
+    separator: &str,
+) -> impl Fn(&str) -> IResult<&str, (CommandInput, u16), nom::error::VerboseError<&str>> + '_ {
+    move |input: &str| separated_pair(parse_command_input, tag(separator), parse_u16)(input)
 }
 
-fn parse_signal(input: &str) -> IResult<&str, Signal> {
+fn parse_command_input(input: &str) -> IResult<&str, CommandInput, nom::error::VerboseError<&str>> {
+    let (input, command_input) =
+        alt((parse_command_input_signal, parse_command_input_wire))(input)?;
+    Ok((input, command_input))
+}
+
+fn parse_command_input_signal(
+    input: &str,
+) -> IResult<&str, CommandInput, nom::error::VerboseError<&str>> {
+    let (input, signal) = parse_signal(input)?;
+    Ok((input, CommandInput::Signal(signal)))
+}
+
+fn parse_command_input_wire(
+    input: &str,
+) -> IResult<&str, CommandInput, nom::error::VerboseError<&str>> {
+    let (input, wire) = parse_wire(input)?;
+    Ok((input, CommandInput::Wire(wire)))
+}
+
+fn parse_signal(input: &str) -> IResult<&str, Signal, nom::error::VerboseError<&str>> {
     let (input, value) = parse_u16(input)?;
     Ok((input, Signal(value)))
 }
 
-fn parse_wire(input: &str) -> IResult<&str, Wire> {
+fn parse_wire(input: &str) -> IResult<&str, Wire, nom::error::VerboseError<&str>> {
     let (input, value) = alpha1(input)?;
     Ok((input, Wire(value.to_string())))
 }
 
-fn parse_u16(input: &str) -> IResult<&str, u16> {
+fn parse_u16(input: &str) -> IResult<&str, u16, nom::error::VerboseError<&str>> {
     map_res(digit1, str::parse)(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_command_input() {
+        let signal = CommandInput::Signal(Signal(15));
+        assert_eq!(signal, parse_command_input("15").unwrap().1);
+
+        let signal = CommandInput::Wire(Wire("abc".to_string()));
+        assert_eq!(signal, parse_command_input("abc").unwrap().1);
+    }
+
+    #[test]
+    fn test_parse_command_and() {
+        let expected = Command::And(
+            CommandInput::Signal(Signal(15)),
+            CommandInput::Wire(Wire("ab".to_string())),
+        );
+        assert_eq!(expected, parse_command_and("15 AND ab").unwrap().1);
+
+        let expected = Command::And(
+            CommandInput::Wire(Wire("ab".to_string())),
+            CommandInput::Signal(Signal(15)),
+        );
+        assert_eq!(expected, parse_command_and("ab AND 15").unwrap().1);
+
+        let expected = Command::And(
+            CommandInput::Wire(Wire("ab".to_string())),
+            CommandInput::Wire(Wire("cd".to_string())),
+        );
+        assert_eq!(expected, parse_command_and("ab AND cd").unwrap().1);
+    }
+
+    #[test]
+    fn test_parse_instruction() {
+        let expected = Instruction {
+            command: Command::And(
+                CommandInput::Signal(Signal(15)),
+                CommandInput::Wire(Wire("ab".to_string())),
+            ),
+            destination: Wire("bc".to_string()),
+        };
+        assert_eq!(expected, parse_instruction("15 AND ab -> bc").unwrap().1);
+    }
 }
