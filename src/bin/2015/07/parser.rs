dca3f9b8511083aa2ad9637b200f57dfbe6c7246
EinMilchBoss <christian.benezuela@gmail.com>
2023-05-15 12:46:28 +0200

    Add test for parser.

--- src/bin/2015/07/parser.rs
+++ src/bin/2015/07/parser.rs
@@ -8,7 +8,7 @@ use nom::{
     Finish, IResult,
 };
 
-type SignalSize = u16;
+type SignalType = u16;
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Instruction {
@@ -23,11 +23,11 @@ pub enum Command {
     Or(CommandInput, CommandInput),
     LeftShift {
         value: CommandInput,
-        shift: SignalSize,
+        shift: SignalType,
     },
     RightShift {
         value: CommandInput,
-        shift: SignalSize,
+        shift: SignalType,
     },
     Not(CommandInput),
 }
@@ -39,10 +39,25 @@ pub enum CommandInput {
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Signal(pub SignalSize);
+pub struct Signal(pub SignalType);
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Wire(pub String);
+
+impl Instruction {
+    pub fn new(command: Command, destination: Wire) -> Self {
+        Self {
+            command,
+            destination,
+        }
+    }
+}
+
+impl Wire {
+    pub fn from_id(id: &str) -> Self {
+        Self(id.to_string())
+    }
+}
 
 pub fn parse_instructions(input: &str) -> Result<Vec<Instruction>, nom::error::VerboseError<&str>> {
     separated_list0(newline, cut(parse_instruction))(input)
@@ -116,7 +131,7 @@ fn parse_separated_command_input_pair(
 fn parse_shift_command(
     separator: &str,
 ) -> impl Fn(&str) -> IResult<&str, (CommandInput, u16), nom::error::VerboseError<&str>> + '_ {
-    move |input: &str| separated_pair(parse_command_input, tag(separator), parse_u16)(input)
+    move |input: &str| separated_pair(parse_command_input, tag(separator), parse_signal_type)(input)
 }
 
 fn parse_command_input(input: &str) -> IResult<&str, CommandInput, nom::error::VerboseError<&str>> {
@@ -140,7 +155,7 @@ fn parse_command_input_wire(
 }
 
 fn parse_signal(input: &str) -> IResult<&str, Signal, nom::error::VerboseError<&str>> {
-    let (input, value) = parse_u16(input)?;
+    let (input, value) = parse_signal_type(input)?;
     Ok((input, Signal(value)))
 }
 
@@ -149,53 +164,132 @@ fn parse_wire(input: &str) -> IResult<&str, Wire, nom::error::VerboseError<&str>
     Ok((input, Wire(value.to_string())))
 }
 
-fn parse_u16(input: &str) -> IResult<&str, u16, nom::error::VerboseError<&str>> {
+fn parse_signal_type(input: &str) -> IResult<&str, SignalType, nom::error::VerboseError<&str>> {
     map_res(digit1, str::parse)(input)
 }
 
 #[cfg(test)]
 mod tests {
+    use pretty_assertions::assert_eq;
+    use rstest::rstest;
+
     use super::*;
 
-    #[test]
-    fn test_parse_command_input() {
-        let signal = CommandInput::Signal(Signal(15));
-        assert_eq!(signal, parse_command_input("15").unwrap().1);
-
-        let signal = CommandInput::Wire(Wire("abc".to_string()));
-        assert_eq!(signal, parse_command_input("abc").unwrap().1);
-    }
-
-    #[test]
-    fn test_parse_command_and() {
-        let expected = Command::And(
-            CommandInput::Signal(Signal(15)),
-            CommandInput::Wire(Wire("ab".to_string())),
-        );
-        assert_eq!(expected, parse_command_and("15 AND ab").unwrap().1);
-
-        let expected = Command::And(
-            CommandInput::Wire(Wire("ab".to_string())),
-            CommandInput::Signal(Signal(15)),
-        );
-        assert_eq!(expected, parse_command_and("ab AND 15").unwrap().1);
-
-        let expected = Command::And(
-            CommandInput::Wire(Wire("ab".to_string())),
-            CommandInput::Wire(Wire("cd".to_string())),
-        );
-        assert_eq!(expected, parse_command_and("ab AND cd").unwrap().1);
-    }
-
-    #[test]
-    fn test_parse_instruction() {
-        let expected = Instruction {
-            command: Command::And(
-                CommandInput::Signal(Signal(15)),
-                CommandInput::Wire(Wire("ab".to_string())),
+    #[rstest]
+    #[case(
+        Instruction::new(Command::Set(CommandInput::Signal(Signal(123))), Wire::from_id("x")),
+        "123 -> x"
+    )]
+    #[case(
+        Instruction::new(
+            Command::Set(CommandInput::Wire(Wire::from_id("x"))),
+            Wire::from_id("y")
+        ),
+        "x -> y"
+    )]
+    #[case(
+        Instruction::new(
+            Command::And(CommandInput::Signal(Signal(123)), CommandInput::Signal(Signal(456))),
+            Wire::from_id("z"),
+        ),
+        "123 AND 456 -> z"
+    )]
+    #[case(
+        Instruction::new(
+            Command::And(
+                CommandInput::Wire(Wire::from_id("x")),
+                CommandInput::Wire(Wire::from_id("y"))
             ),
-            destination: Wire("bc".to_string()),
-        };
-        assert_eq!(expected, parse_instruction("15 AND ab -> bc").unwrap().1);
+            Wire::from_id("z"),
+        ),
+        "x AND y -> z"
+    )]
+    #[case(
+        Instruction::new(
+            Command::Or(CommandInput::Signal(Signal(123)), CommandInput::Signal(Signal(456))),
+            Wire::from_id("z"),
+        ),
+        "123 OR 456 -> z"
+    )]
+    #[case(
+        Instruction::new(
+            Command::Or(
+                CommandInput::Wire(Wire::from_id("x")),
+                CommandInput::Wire(Wire::from_id("y"))
+            ),
+            Wire::from_id("z"),
+        ),
+        "x OR y -> z"
+    )]
+    #[case(
+        Instruction::new(
+            Command::LeftShift {
+                value: CommandInput::Signal(Signal(123)),
+                shift: 2
+            },
+            Wire::from_id("z"),
+        ),
+        "123 LSHIFT 2 -> z"
+    )]
+    #[case(
+        Instruction::new(
+            Command::LeftShift {
+                value: CommandInput::Wire(Wire::from_id("x")),
+                shift: 2
+            },
+            Wire::from_id("z"),
+        ),
+        "x LSHIFT 2 -> z"
+    )]
+    #[case(
+        Instruction::new(
+            Command::RightShift {
+                value: CommandInput::Signal(Signal(123)),
+                shift: 2
+            },
+            Wire::from_id("z"),
+        ),
+        "123 RSHIFT 2 -> z"
+    )]
+    #[case(
+        Instruction::new(
+            Command::RightShift {
+                value: CommandInput::Wire(Wire::from_id("x")),
+                shift: 2
+            },
+            Wire::from_id("z"),
+        ),
+        "x RSHIFT 2 -> z"
+    )]
+    #[case(
+        Instruction::new(Command::Not(CommandInput::Signal(Signal(123))), Wire::from_id("z"),),
+        "NOT 123 -> z"
+    )]
+    #[case(
+        Instruction::new(
+            Command::Not(CommandInput::Wire(Wire::from_id("x"))),
+            Wire::from_id("z"),
+        ),
+        "NOT x -> z"
+    )]
+    fn parse_instruction_test_all_valid_cases(#[case] expected: Instruction, #[case] input: &str) {
+        let (_, actual) = parse_instruction(input).unwrap();
+        assert_eq!(expected, actual);
+    }
+
+    #[rstest]
+    #[case("123")]
+    #[case("x")]
+    #[case("NOT")]
+    #[case("Invalid Input")]
+    fn parse_instruction_test_if_err_for_invalid_case(#[case] input: &str) {
+        assert_eq!(parse_instruction(input).is_err(), true);
+    }
+
+    #[test]
+    fn parse_instructions_test_if_err_for_invalid_input() {
+        let input = vec!["123 -> x", "Something invalid", "NOT x -> y"].join("\n");
+
+        assert_eq!(parse_instructions(&input).is_err(), true);
     }
 }
